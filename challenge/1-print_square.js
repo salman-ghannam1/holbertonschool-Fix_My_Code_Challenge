0da4239ab4f7203fd978279308cb96500eb0a0b4
salman_ghannam1 <sloome222@gmail.com>
2026-06-21 12:38:26 +0300

    f

--- challenge/1-print_square.js
+++ challenge/1-print_square.js
@@ -3,19 +3,12 @@
  * Print a square using the character #
  */
 
-if (process.argv.length <= 2) {
+const size = parseInt(process.argv[2], 10);
+
+if (isNaN(size)) {
   console.log('Missing size');
-  process.exit(1);
-}
-
-const size = parseInt(process.argv[2]);
-
-for (let i = 0; i < size; i++) {
-  let row = '';
-
-  for (let j = 0; j < size; j++) {
-    row += '#';
+} else {
+  for (let i = 0; i < size; i++) {
+    console.log('#'.repeat(size));
   }
-
-  console.log(row);
 }
